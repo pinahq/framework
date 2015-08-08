2fac566f3d2d245f5f65d66c024edb5610bab1b2
Alex Yashin <alex-yashin@ya.ru>
2015-08-08 17:39:18 +0400

    добавляем контекст в роутинг

--- src/Pina/helpers/block.iflocation.php
+++ src/Pina/helpers/block.iflocation.php
@@ -1,13 +1,14 @@
 <?php
 
 use Pina\Url;
+use Pina\Route;
 
 function smarty_block_iflocation($params, $content, &$view, &$repeat)
 {
 	if (empty($content) || empty($params['get'])) return '';
 	
 	$pattern = Url::trim($params['get']);
-    $needed = Url::resource($pattern, $params);
+    $needed = Route::resource($pattern, $params);
 
 	if (empty($needed)) return '';
 	
