f89a3c5b995cdd1527603546fd88129bc5b40114
Alex Yashin <alex-yashin@ya.ru>
2015-09-20 01:54:37 +0400

    форматирование даты

--- src/Pina/helpers/modifier.format_date.php
+++ src/Pina/helpers/modifier.format_date.php
@@ -1,21 +1,9 @@
 <?php
 
 use Pina\Config;
+use Pina\Date;
 
 function smarty_modifier_format_date($date)
 {
-	if (empty($date) || $date == '0000-00-00 00:00:00' || $date == "0000-00-00")
-	{
-		return '-';
-	}
-
-	if (is_string($date))
-	{
-		$date = strtotime($date);
-	}
-	
-	$f = Config::get("appearance", "date_format");
-	if (empty($f)) $f = "d.m.Y";
-	
-	return date($f, $date);
+	return Date::format($date);
 }
