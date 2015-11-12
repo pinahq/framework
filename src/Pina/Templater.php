cfea9e34106a079f805eaebcf8e9fe1e95051a79
Alex Yashin <alex-yashin@ya.ru>
2015-11-12 18:16:34 +0300

    wrapper теперь работает со всеми тегами, не только с div

--- src/Pina/Templater.php
+++ src/Pina/Templater.php
@@ -4,7 +4,7 @@ namespace Pina;
 
 class Templater extends \Smarty
 {
-    
+
     private $layout = 'main';
 
     public function __construct()
@@ -13,9 +13,9 @@ class Templater extends \Smarty
 
         $this->strict_resources = array();
         array_unshift(
-            $this->plugins_dir, __DIR__ . '/helpers'
+                $this->plugins_dir, __DIR__ . '/helpers'
         );
-        
+
         $paths = Module::paths('helpers');
         foreach ($paths as $v) {
             if (is_dir($v)) {
@@ -34,12 +34,12 @@ class Templater extends \Smarty
         }
         $this->template_dir[] = App::path() . "/default/";
 
-        $this->compile_dir = App::templaterCompiled() .'/'. md5(Site::key() . ":" . $template);
+        $this->compile_dir = App::templaterCompiled() . '/' . md5(Site::key() . ":" . $template);
         @mkdir($this->compile_dir);
 
         $this->cache_dir = App::templaterCache();
     }
-    
+
     public function setLayout($layout = "page")
     {
         if (empty($layout)) {
@@ -48,7 +48,7 @@ class Templater extends \Smarty
 
         $this->layout = $layout;
     }
-    
+
     public function getLayout()
     {
         return $this->layout;
@@ -69,7 +69,7 @@ class Templater extends \Smarty
         if (!isset($params['get'])) {
             return '';
         }
-        
+
         $params['get'] = Route::resource($params['get'], $params);
 
         if (!Request::isAvailable($params['get'], 'get')) {
@@ -77,7 +77,7 @@ class Templater extends \Smarty
         }
 
         $vars_backup = $view->_tpl_vars;
-        
+
         if (is_array($params)) {
             foreach ($params as $name => $value) {
                 $view->assign($name, $value);
@@ -95,14 +95,15 @@ class Templater extends \Smarty
         if (!empty($params['display'])) {
             $handler .= '.' . $params['display'];
         }
-        
+
         $view->assign('params', $params);
         $result = $view->fetch('file:' . $handler . '.tpl');
 
         $view->_tpl_vars = $vars_backup;
 
-        if (!empty($params['wrapper']) && !empty($result)) {
-            return '<div class="' . $params['wrapper'] . '">' . $result . '</div>';
+        if (!empty($params['wrapper']) && $result) {
+            list($start, $end) = static::wrapper($params['wrapper']);
+            return $start . $result . $end;
         }
 
         return $result;
@@ -118,7 +119,7 @@ class Templater extends \Smarty
         }
 
         $vars_backup = $view->_tpl_vars;
-        
+
         $params['get'] = Route::resource($params['get'], $params);
         $result = Request::internal($params['get'], 'get', $params);
 
@@ -130,10 +131,49 @@ class Templater extends \Smarty
         $view->_tpl_vars = $vars_backup;
 
         if (!empty($params['wrapper']) && $result) {
-            return '<div class="' . $params['wrapper'] . '">' . $result . '</div>';
+            list($start, $end) = static::wrapper($params['wrapper']);
+            return $start . $result . $end;
         }
 
         return $result;
+    }
+
+    static private function wrapper($rest)
+    {
+        if (empty($rest)) {
+            return array('', '');
+        }
+        $params = array();
+        while (1) {
+            $pos = strrpos($rest, '=');
+            if ($pos === false) {
+                break;
+            }
+            $value = trim(substr($rest, $pos + 1), ' \'"');
+            $rest = trim(substr($rest, 0, $pos));
+            $pos = strrpos($rest, ' ');
+            if ($pos === false) {
+                break;
+            }
+            $key = trim(substr($rest, $pos + 1));
+            $rest = trim(substr($rest, 0, $pos));
+
+            array_push($params, ' ' . $key . '="' . $value . '"');
+
+            $i++;
+            if ($i > 10) {
+                break;
+            }
+        }
+        $tag = trim($rest);
+
+        $start = '<' . $tag;
+        while ($p = array_pop($params)) {
+            $start .= $p;
+        }
+        $start .= '>';
+        $end = '</' . $tag . '>';
+        return array($start, $end);
     }
 
 }
