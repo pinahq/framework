86657235df23f3bee13a53c63b9c5510fa5e4fc0
Alex Yashin <alex-yashin@ya.ru>
2021-08-31 17:38:15 +0400

    контрол Wrapper

---
+++ src/Pina/Controls/Wrapper.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Pina\Controls;
+
+
+use Pina\Html;
+
+class Wrapper extends Control
+{
+    protected $path = '';
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    protected function draw()
+    {
+        return Html::nest($this->path, $this->compile());
+    }
+
+}
