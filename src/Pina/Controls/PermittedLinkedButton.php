ef7a297c5f420f59d59a0211adfa2454270f38b4
Alex Yashin <alex-yashin@ya.ru>
2022-03-31 00:04:10 +0400

    кнопка, которая показывается если только ее URL разрешен

---
+++ src/Pina/Controls/PermittedLinkedButton.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pina\Controls;
+
+use Pina\Access;
+use Pina\App;
+
+class PermittedLinkedButton extends LinkedButton
+{
+    protected function draw()
+    {
+        $parsed = parse_url($this->link);
+        if ($parsed['host'] == App::host()) {
+            if (!Access::isPermitted($parsed['path'])) {
+                return '';
+            }
+        }
+        return parent::draw();
+    }
+}
