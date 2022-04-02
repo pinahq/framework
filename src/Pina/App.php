e42437e12f0b54b30ef130c1a9f32c9192e377eb
Alex Yashin <alex-yashin@ya.ru>
2022-04-02 11:29:46 +0400

    удаляем HiddenType

--- src/Pina/App.php
+++ src/Pina/App.php
@@ -66,8 +66,6 @@ class App
         $types->share('timestamp', Types\TimestampType::class);
         $types->share('uuid', Types\UUIDType::class);
         $types->share('blob', Types\BlobType::class);
-        $types->share('hidden', Types\HiddenType::class);
-        $types->share('static', Types\StaticType::class);
         static::$container->share('types', $types);
 
         $events = new Container();
