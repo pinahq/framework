61f8b9da03fca6f5dd3fb7e1d6c6b2b507372ea4
Alex Yashin <alex-yashin@ya.ru>
2023-01-03 13:32:58 +0400

    вложенные роутеры

--- src/Pina/Router.php
+++ src/Pina/Router.php
@@ -44,7 +44,7 @@ class Router
 
         $action .= $this->calcDeeperAction($resource, $this->patterns[$c]);
 
-        return method_exists($this->endpoints[$c], $action);
+        return method_exists($this->endpoints[$c], $action) || method_exists($this->endpoints[$c], 'router');
     }
 
     /**
@@ -88,6 +88,13 @@ class Router
         $action .= $this->calcDeeperAction($resource, $pattern);
 
         if (!method_exists($inst, $action)) {
+
+            if (method_exists($inst, 'router')) {
+                /** @var Router $router */
+                $router = $inst->router();
+                return $router->run($resource, $method, $data);
+            }
+
             throw new NotFoundException();
         }
 
