def08a3f01f146dc742e684162daa59d74ad0aa8
Alex Yashin <alex-yashin@ya.ru>
2023-01-28 00:45:20 +0400

    поправил отправку ошибки через метод addError

--- src/Pina/BadRequestException.php
+++ src/Pina/BadRequestException.php
@@ -13,7 +13,9 @@ class BadRequestException extends RuntimeException
     public function __construct($message = "", $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
-        $this->addError($message, $code);
+        if (!empty($message)) {
+            $this->addError($message, $code);
+        }
     }
 
     public function setErrors($errors)
