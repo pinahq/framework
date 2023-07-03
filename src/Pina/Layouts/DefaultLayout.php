ae774ab99430ae5f5a3c4bf77c43524f73700d91
Alex Yashin <alex-yashin@ya.ru>
2023-07-03 13:52:43 +0400

    функциональный layout по умолчанию

--- src/Pina/Layouts/DefaultLayout.php
+++ src/Pina/Layouts/DefaultLayout.php
@@ -2,42 +2,170 @@
 
 namespace Pina\Layouts;
 
+use Exception;
 use Pina\App;
 use Pina\Controls\Control;
 use Pina\Html;
+use Pina\Request;
 
 class DefaultLayout extends Control
 {
 
+    /**
+     * @return string
+     * @throws Exception
+     */
     protected function draw()
     {
+        $this->loadResources();
+
         //вначале генерируем тег body, чтобы все контролы внутри body смогли зарегистировать css перед генерацией head
-        $body = Html::tag('body', $this->makeBody());
-        $head = Html::tag('head', $this->makeMeta() . $this->makeCss());
-        $html = Html::tag('html', $head . $body . $this->makeJs());
-        return '<!DOCTYPE html>' . "\n" . $html;
+        $body = Html::tag('body', $this->drawBody());
+        $head = Html::tag('head', $this->drawHead());
+        return '<!DOCTYPE html>' . "\n" . Html::tag('html', $head . $body . $this->drawJs());
     }
 
-    protected function makeBody()
+    protected function loadResources()
     {
-        return $this->drawInnerBefore() . $this->drawInner() . $this->drawInnerAfter();
+
     }
 
-    protected function makeCss()
+    /**
+     * @return string
+     * @throws Exception
+     */
+    protected function drawBody()
+    {
+        return $this->drawHeader()
+            . $this->drawPageHeader()
+            . $this->drawInnerBefore()
+            . $this->drawInner()
+            . $this->drawInnerAfter()
+            . $this->drawFooter();
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    protected function drawHeader()
+    {
+        return Html::zz('header.container section(a.logo[href=/](img[src=/logo.png]))');
+    }
+
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    protected function drawPageHeader()
+    {
+        $title = strval(Request::getPlace('page_header'));
+        if (empty($title)) {
+            return '';
+        }
+        $breadcrumb = Request::getPlace('breadcrumb');
+        return Html::zz('nav.breadcrumbs(.container(%+header(h1%)))', $breadcrumb, $title);
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    protected function drawFooter()
+    {
+        return Html::zz('footer.container%', 'Made on PinaFramework (c) Alex Yashin');
+    }
+
+    protected function drawHead()
+    {
+        return Html::tag(
+            'head',
+            $this->drawTitle()
+            . $this->drawMeta()
+            . $this->drawIcon()
+            . $this->drawCss()
+            . $this->drawHeadCounters()
+        );
+    }
+
+    protected function drawMeta()
+    {
+        return $this->drawCharset()
+            . $this->drawCanonical()
+            . $this->drawContentMeta()
+            . $this->drawBrowserMeta();
+    }
+
+    protected function drawCharset()
+    {
+        return $this->drawMetaLine(['charset' => 'UTF-8']);
+    }
+
+    protected function drawCanonical()
+    {
+        $resource = Request::getPlace('canonical');
+        if (empty($resource)) {
+            return '';
+        }
+
+        return Html::tag('link', '', ['rel' => 'canonical', 'href' => App::link($resource)]);
+    }
+
+    protected function drawContentMeta()
+    {
+        return $this->drawMetaLine(['name' => 'description', 'content' => Request::getPlace('meta_description')])
+            . $this->drawMetaLine(['name' => 'keywords', 'content' => Request::getPlace('meta_keywords')])
+            . $this->drawMetaLine(['property' => 'og:type', 'content' => Request::getPlace('og_type')])
+            . $this->drawMetaLine(['property' => 'og:title', 'content' => Request::getPlace('og_title')])
+            . $this->drawMetaLine(['property' => 'og:description', 'content' => Request::getPlace('og_description')])
+            . $this->drawMetaLine(['property' => 'og:image', 'content' => Request::getPlace('og_image')]);
+    }
+
+    protected function drawBrowserMeta()
+    {
+        return
+            $this->drawMetaLine(['http-equiv' => 'X-UA-Compatible', 'content' => 'IE=edge'])
+            . $this->drawMetaLine(
+                [
+                    'name' => "viewport",
+                    'content' => "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+                ]
+            );
+    }
+
+    protected function drawMetaLine($attributes)
+    {
+        if ((!empty($attributes['name']) || !empty($attributes['property'])) && empty($attributes['content'])) {
+            return '';
+        }
+        return Html::tag('meta', '', $attributes);
+    }
+
+
+    protected function drawTitle()
+    {
+        return Html::tag('title', Request::getPlace('page_header'));
+    }
+
+    protected function drawIcon()
+    {
+        return '';
+    }
+
+    protected function drawHeadCounters()
+    {
+        return '';
+    }
+
+    protected function drawCss()
     {
         return App::assets()->fetch('css');
     }
 
-    protected function makeJs()
+    protected function drawJs()
     {
         return App::assets()->fetch('js');
-    }
-
-    protected function makeMeta()
-    {
-        return Html::tag('meta', '', ['charset' => 'utf8'])
-            . Html::tag('meta', '', ['http-equiv' => 'X-UA-Compatible', 'content' => 'IE=edge'])
-            . Html::tag('meta', '', ['name' => "viewport", 'content' => "width=device-width, initial-scale=1"]);
     }
 
 }
