7fedd46767618dfb9a6ba98de843ffdc6d78dc36
Alex Yashin <alex-yashin@ya.ru>
2023-10-13 23:37:41 +0400

    fix

--- src/Pina/Types/FormattedTextType.php
+++ src/Pina/Types/FormattedTextType.php
@@ -8,8 +8,8 @@ class FormattedTextType extends TextType
 
     public function draw($value): string
     {
-        $value = preg_replace('/(http[s]{0,1}\:\/\/\S{4,})(\s{0,})/ims', '<a href="$1" target="_blank">$1</a>$2', $value);
         $value = htmlentities($value);
+        $value = preg_replace('/(http[s]{0,1}\:\/\/\S{4,})(\s{0,})/ims', '<a href="$1" target="_blank">$1</a>$2', $value);
         $value = parent::draw($value);
 
         return $value;
