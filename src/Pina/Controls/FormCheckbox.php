a393fa115f9c5db0fd001a6dac5f941867222b7f
Alex Yashin <alex-yashin@ya.ru>
2023-12-23 21:00:40 +0400

    откатываем FormCheckbox для режима с множественными значениями

--- src/Pina/Controls/FormCheckbox.php
+++ src/Pina/Controls/FormCheckbox.php
@@ -24,20 +24,18 @@ class FormCheckbox extends FormInput
     protected function drawInput()
     {
         $options = ['type' => $this->type, 'value' => $this->checkedValue];
-        $id = uniqid('ch');
 
         if ($this->name) {
             $options['name'] = $this->name;
-            $options['id'] = $id;
+            $options['id'] = $this->name;
         }
 
-        $selected = is_array($this->value) ? in_array($this->checkedValue, $this->value) : !is_null($this->value) && $this->value == $this->checkedValue;
-        if ($selected) {
+        if ($this->value == $this->checkedValue) {
             $options['checked'] = 'checked';
         }
 
         $r = Html::tag('input', '', $options);
-        $r .= Html::tag('label', $this->compact ? '' : $this->title, ['for' => $id]);
+        $r .= Html::tag('label', $this->compact ? '' : $this->title, ['for' => $this->name]);
         return $r;
     }
 
