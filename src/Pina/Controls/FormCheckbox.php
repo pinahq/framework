0fb6bbd5971033d344a0419e7f0b5079195d9639
Alex Yashin <alex-yashin@ya.ru>
2023-12-23 20:58:30 +0400

    FormCheckbox должен уметь работать в режиме с множественными вариантами

--- src/Pina/Controls/FormCheckbox.php
+++ src/Pina/Controls/FormCheckbox.php
@@ -24,18 +24,20 @@ class FormCheckbox extends FormInput
     protected function drawInput()
     {
         $options = ['type' => $this->type, 'value' => $this->checkedValue];
+        $id = uniqid('ch');
 
         if ($this->name) {
             $options['name'] = $this->name;
-            $options['id'] = $this->name;
+            $options['id'] = $id;
         }
 
-        if ($this->value == $this->checkedValue) {
+        $selected = is_array($this->value) ? in_array($this->checkedValue, $this->value) : !is_null($this->value) && $this->value == $this->checkedValue;
+        if ($selected) {
             $options['checked'] = 'checked';
         }
 
         $r = Html::tag('input', '', $options);
-        $r .= Html::tag('label', $this->compact ? '' : $this->title, ['for' => $this->name]);
+        $r .= Html::tag('label', $this->compact ? '' : $this->title, ['for' => $id]);
         return $r;
     }
 
