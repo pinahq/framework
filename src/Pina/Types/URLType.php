96083b534d77ec5312830f82d44b9405461cec4d
Alex Yashin <alex-yashin@ya.ru>
2024-03-01 20:20:54 +0400

    URLType

---
+++ src/Pina/Types/URLType.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Pina\Types;
+
+use Pina\App;
+use Pina\Html;
+
+use function Pina\__;
+
+class URLType extends StringType
+{
+
+    public function normalize($value, $isMandatory)
+    {
+        $value = parent::normalize($value, $isMandatory);
+
+        //проверка соответствия isMandatory и $value происходит выше
+        if (!empty($value) && !filter_var($value, FILTER_VALIDATE_URL)) {
+            throw new ValidateException(__("Укажите корректный URL"));
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param mixed $value
+     * @return string
+     */
+    public function draw($value): string
+    {
+        $options = [];
+        if ($this->isExternalLink($value ?? '')) {
+            $options['target'] = '_blank';
+        }
+        return Html::a($value, $value, $options);
+    }
+
+    protected function isExternalLink(string $link)
+    {
+        $host = parse_url($link, PHP_URL_HOST);
+        return !empty($host) && $host != App::host();
+    }
+
+}
