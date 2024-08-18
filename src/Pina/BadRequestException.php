65a21f1e0bab6b1f8492b102acf74c2d83976a45
Alex Yashin <alex-yashin@ya.ru>
2024-08-18 15:42:30 +0400

    fix

--- src/Pina/BadRequestException.php
+++ src/Pina/BadRequestException.php
@@ -12,9 +12,11 @@ class BadRequestException extends RuntimeException
 
     public function __construct($message = "", $code = 0, Throwable $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        //вызов addError дополнит системное поле $this->message, поэтому в конструктор передавать не надо,
+        // чтобы не дублировать
+        parent::__construct('', $code, $previous);
         if (!empty($message)) {
-            $this->addError($message, $code);
+            $this->addError($message, '');
         }
     }
 
