9b5e65281b9bd169ecdfc1817aa8ed3dab82e752
Alex Yashin <alex-yashin@ya.ru>
2024-12-26 09:24:45 +0400

    добавляю простой класс для работы с JSON API

---
+++ src/Pina/Http/JsonAPI.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Pina\Http;
+
+class JsonAPI
+{
+
+    protected $endpoint = '';
+    protected $headers = [];
+    protected $info = false;
+
+    public function __construct(string $endpoint, array $headers = [])
+    {
+        $this->endpoint = $endpoint;
+        $this->headers = $headers;
+    }
+
+    public function get($api)
+    {
+        return $this->request('get', $api);
+    }
+
+    public function post($api, $packet = null)
+    {
+        return $this->request('post', $api, $packet);
+    }
+
+    public function request($method, $api, $packet = null)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->resolveResource($api));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array_merge($this->makeHeaders($packet), $this->headers));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if ($method == 'post') {
+            curl_setopt($ch, CURLOPT_POST, true);
+        }
+        if (!is_null($packet)) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $this->encodePacket($packet));
+        }
+
+        $result = curl_exec($ch);
+        $result = json_decode($result, true);
+
+        $this->info = curl_getinfo($ch);
+        curl_close($ch);
+
+        return $this->processResponse($result);
+    }
+
+    public function isSuccess()
+    {
+        return ($this->info['http_code'] ?? '') == 200;
+    }
+
+    protected function makeHeaders($packet)
+    {
+        return [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+    }
+
+    protected function encodePacket($packet)
+    {
+        return json_encode($packet, JSON_UNESCAPED_UNICODE);
+    }
+
+    protected function resolveResource($api)
+    {
+        return rtrim($this->endpoint, '/') . '/' . ltrim($api, '/');
+    }
+
+    protected function processResponse($response)
+    {
+        return $response;
+    }
+
+}
