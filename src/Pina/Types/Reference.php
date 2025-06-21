dac20dd3f4ef80504d2f0a2bb6994d2394b05c82
Alex Yashin <alex-yashin@ya.ru>
2025-06-21 20:34:02 +0400

    тип-ссылка на другую таблицу

---
+++ src/Pina/Types/Reference.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pina\Types;
+
+use Pina\App;
+use Pina\TableDataGateway;
+
+class Reference extends QueryDirectoryType
+{
+    /** @var TableDataGateway */
+    protected $query;
+
+    public function __construct(TableDataGateway $query)
+    {
+        $this->query = $query;
+    }
+
+    protected function makeQuery(): TableDataGateway
+    {
+        return clone $this->query;
+    }
+
+    public function getSQLType(): string
+    {
+        $pk = $this->query->getSinglePrimaryKey();
+        $fieldset = $this->query->getSchema()->fieldset([$pk])->makeSchema();
+        foreach ($fieldset as $f) {
+            return App::type($f->getType())->getSQLType();
+        }
+
+        throw new \Exception(get_class($this->query) . ' single PK has not been found');
+    }
+}
